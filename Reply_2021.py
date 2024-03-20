d04df359b9bc1df9543bccb16aab463adce46b85
chrisgalanis <chrisgalan2004@gmail.com>
2024-03-20 22:05:53 +0200

    Initial

---
+++ Reply_2021.py
@@ -0,0 +1,12 @@
+
+from collections import OrderedDict
+import math
+
+def printf(smth, mess=""):
+    print(str(mess) + str(smth) + "\n")
+    
+file = open('../TXT/REPLY.txt').read()
+
+file = file.split('\n')
+
+
