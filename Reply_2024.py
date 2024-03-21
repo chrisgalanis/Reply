ad432c5d0ad1d71a4fb5191bd4f759070d369f96
chrisgalanis <50170911+chrisgalanis@users.noreply.github.com>
2024-03-21 18:11:32 +0200

    Add files via upload

--- Reply_2024.py
+++ Reply_2024.py
@@ -2,6 +2,21 @@ def printf(smth, mess=""):
     print(str(mess) + str(smth) + "\n")
 
 
+class GoldenPoint:
+    x = 0
+    y = 0
+
+class SilverPoint:
+    x = 0
+    y = 0
+    sc = 0 #score
+
+class Tiles:
+    id = 0
+    cost = 0
+    number = 0
+
+
 class Input:
     def __init__(self):
         file = open('REPLY.txt').read()
@@ -10,29 +25,53 @@ class Input:
 
         # self.printf(file)
 
-        W,H = file[0].split(' ')
+        W,H, Gn, Sm, Tl = file[0].split(' ')
         W = int(W)
         H = int(H)
+        Gn = int(Gn)
+        Sm = int(Sm)
+        Tl = int(Tl)
+
         printf(W,"W: ")
         printf(H, "H: ")
+
         file = file[1:]
 
-        N,M,R = file[0].split(' ')
-        N = int(N)
-        M = int(M)
-        R = int(R)
-        printf(N,"N: ")
-        printf(M,"M: ")
-        printf(R,"R: ")
+        GPList = []
+       
+
+        for i in range(0,Gn):
+            line = file[i]
+            gp = GoldenPoint()
+            gp.x = line[0]
+            gp.y = line[1]
+            GPList.append(gp)
+
+        printf(GPList, "GPList: ")
+
+        file = file[Gn:]
+        
+        SPList = []
+        for i in range(0,Sm):
+            line = file[i]
+            sp = SilverPoint()
+            sp.x = line[0]
+            sp.y = line[1]
+            sp.sc = line[2]
+            SPList.append(sp)
+
+        for i in range(0,Tl): 
+            line = file[i]
+            tl = SilverPoint()
+            tl.id = line[0]
+            tl.y = line[1]
+            sp.sc = line[2]
+            SPList.append(sp)
+
         file = file[1:]
         
 
-        bx = []
-        by = []
-        bl = []
-        bc = []
-        ar = []
-        ac = []
+       
 
 
 
